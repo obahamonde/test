af43a01dbc2caac9707ca202f8d560a82987b3f2
Oscar Bahamonde <107950590+obahamonde@users.noreply.github.com>
2023-03-23 01:31:33 -0500

    added template

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify({'message': 'Hello, World!'})
-
+    if "theme" in request.args:
+        theme = request.args["theme"]
+    else:
+        theme = "light"
+    if "name" in request.args:
+        name = request.args["name"]
+    else:
+        name = "World"
+    return render_template("index.html", theme=theme, name=name)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
