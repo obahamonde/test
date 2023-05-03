0b364ebb1513f0421406c36021c6bd598e48f92a
@obahamonde <oscarmartinbahamondemunoz@gmail.comroot@worries:~>
2023-05-03 10:55:29 -0500

    updated Dockerfile

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from aiofauna import Api
-
+from aiohttp.web import run_app
 
 app = Api()
 
 @app.get("/")
 async def index():
     return {"hello": "world"}
+
+
+if __name__ == '__main__':
+    run_app(app, port=8080)
