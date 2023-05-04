55a58f80e913f9f0485b79d343da6fd26e336948
@obahamonde <oscarmartinbahamondemunoz@gmail.comroot@worries:~>
2023-05-04 18:18:05 -0500

    stripped

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 from aiofauna import Api
-from aiohttp.web import run_app
+
 
 app = Api()
 
 @app.get("/")
 async def index():
     return {"hello": "world"}
-
-
-if __name__ == '__main__':
-    run_app(app, port=8080)
