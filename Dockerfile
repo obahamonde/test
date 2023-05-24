2b7a3ba455e46e2a1ee81db4d0147b7693cc3490
@obahamonde <oscarmartinbahamondemunoz@gmail.com>
2023-05-24 11:21:58 +0000

    2

--- Dockerfile
+++ Dockerfile
@@ -8,4 +8,4 @@ COPY ${LOCAL_PATH} /app
 
 RUN pip install -r requirements.txt
 
-CMD ["adev", "runserver", "--port", "8080","--livereload"]
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from aiofauna import Api
 
-
 app = Api()
 
 @app.get("/")
 async def index():
     return {"hello": "world"}
+
+@app.get("/hello/{name}")
+async def hello(name: str):
+    return {"hello": name}
+
+if __name__ == "__main__":
+    app.run()
